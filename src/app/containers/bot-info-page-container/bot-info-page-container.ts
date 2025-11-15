ecb1704766c1358381e401ef11f814875b2e8ee3
NikolayTola <kolyatola@mail.ru>
2025-11-15 08:40:00 +0700

    added navigate "back"

--- src/app/containers/bot-info-page-container/bot-info-page-container.ts
+++ src/app/containers/bot-info-page-container/bot-info-page-container.ts
@@ -5,12 +5,15 @@ import {AgGridErrorList} from '../../components/ag-grid-error-list/ag-grid-error
 import {TitleTableLayout} from '../../components/title-table-layout/title-table-layout';
 import {AgGridBotDataList} from '../../components/ag-grid-bot-data-list/ag-grid-bot-data-list';
 import {AsyncPipe} from '@angular/common';
-import {getDataActiveBot} from '../../+state/servers/servers.selectors';
+import {getActiveServerIpPort, getDataActiveBot} from '../../+state/servers/servers.selectors';
 import {TitleContentLayout} from '../../components/title-content-layout/title-content-layout';
 import {Actions} from '../../components/ag-grid-components/actions/actions';
+import {ActivatedRoute, Router} from '@angular/router';
+import {take} from 'rxjs';
+import {clearActiveElementData, setActiveServer} from '../../+state/servers/servers.actions';
 
 @Component({
-  selector: 'app-bot-info-page-container',
+    selector: 'app-bot-info-page-container',
     imports: [
         AgGridBotsControl,
         AgGridErrorList,
@@ -20,27 +23,50 @@ import {Actions} from '../../components/ag-grid-components/actions/actions';
         TitleContentLayout,
         Actions,
     ],
-  standalone: true,
-  templateUrl: './bot-info-page-container.html',
-  styleUrl: './bot-info-page-container.scss',
+    standalone: true,
+    templateUrl: './bot-info-page-container.html',
+    styleUrl: './bot-info-page-container.scss',
 })
 export class BotInfoPageContainer implements OnInit {
-  private store = inject(Store)
-  data = {
-    errors: [],
-    params: []
-  };
+    private store = inject(Store);
+    private router = inject(Router);
 
-  dataActiveBot$ = this.store.select(getDataActiveBot)
+    private route=inject(ActivatedRoute);
 
-  ngOnInit() {
-    console.log('тут вызвать диспатч - делаем следующее:')
+    dataActiveBot$ = this.store.select(getDataActiveBot)
+    ipPort$ = this.store.select(getActiveServerIpPort);
 
-    console.log('сохраняю id в стейт')
-    console.log('в эффекте делаю запросы на данные о боте, его ответы, его ошибки по ip, port и id из стэйта')
-  }
 
-  events($event: any) {
-    console.log($event)
-  }
+    ngOnInit() {
+        console.log('тут вызвать диспатч - делаем следующее:')
+
+        console.log('сохраняю id в стейт')
+        console.log('в эффекте делаю запросы на данные о боте, его ответы, его ошибки по ip, port и id из стэйта')
+
+        this.route.paramMap.subscribe(params => {
+            const ipPort = params.get('ipPort');
+
+            if (ipPort !== null) {
+                const [ip, port] = ipPort.split(':');
+                this.store.dispatch(clearActiveElementData());
+                this.store.dispatch(setActiveServer({ ip, port }));
+            }
+        });
+    }
+
+    events($event: any) {
+        console.log($event)
+    }
+
+    onAction($event: any, note: string) {
+        if ($event.event === 'Actions:ACTION_CLICKED') {
+            if (note === 'reply') {
+                this.ipPort$.pipe(take(1)).subscribe(ipPort => {
+                    if (ipPort) {
+                        this.router.navigate([`/server/${ipPort}/tab/bots`]);
+                    }
+                });
+            }
+        }
+    }
 }
