907903a5676cac8fa337859f509ce0cbe7da6328
NikolayTola <kolyatola@mail.ru>
2025-11-27 10:45:21 +0700

    refactor job form container; improve button state management and update jobParams handling

--- src/app/containers/job-form-container/job-form-container.ts
+++ src/app/containers/job-form-container/job-form-container.ts
@@ -10,6 +10,7 @@ import {getInfoActiveBot} from '../../+state/servers/servers.selectors';
 import {AsyncPipe, JsonPipe} from '@angular/common';
 import {setBotSettings} from '../../+state/servers/servers.actions';
 import {take} from 'rxjs';
+import {IBotInfo} from '../../models/servers';
 
 @Component({
   selector: 'app-job-form-container',
@@ -31,11 +32,13 @@ export class JobFormContainer implements OnInit {
   private store = inject(Store)
 
   jobInfo$ = this.store.select(getInfoActiveBot);
-  newJobInfo: any = null;
+  newJobInfo: string | null = null;
+  allSettings!: IBotInfo;
 
   ngOnInit() {
     this.jobInfo$.pipe(take(1)).subscribe(jobInfo => {
-      this.newJobInfo = JSON.stringify(jobInfo, null, 2);
+      this.allSettings = structuredClone(jobInfo);
+      this.newJobInfo = JSON.stringify(jobInfo.jobParams, null, 2);
     });
   }
 
@@ -44,14 +47,29 @@ export class JobFormContainer implements OnInit {
   }
 
   clickButton(note: string) {
+
     if (note === 'Bot set') {
-      const parsed = JSON.parse(this.newJobInfo);
-      this.store.dispatch(setBotSettings({ id: parsed.id, settings: this.newJobInfo }));
+      const parsedJobParams = JSON.parse(this.newJobInfo!);
+
+      const newSettingsObject: IBotInfo = {
+        ...this.allSettings,
+        jobParams: parsedJobParams
+      };
+
+      const newSettingsJson: string = JSON.stringify(newSettingsObject);
+
+      this.store.dispatch(
+        setBotSettings({
+          id: this.allSettings.id,
+          settings: newSettingsJson
+        })
+      );
     }
 
     else if (note === 'Reset') {
       this.jobInfo$.pipe(take(1)).subscribe(jobInfo => {
-        this.newJobInfo = JSON.stringify(jobInfo, null, 2);
+        this.allSettings = structuredClone(jobInfo);
+        this.newJobInfo = JSON.stringify(jobInfo.jobParams, null, 2);
       });
     }
   }
