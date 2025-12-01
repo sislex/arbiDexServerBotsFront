edf9aa4e4fc15ef626aff977526f4f2084426e89
NikolayTola <kolyatola@mail.ru>
2025-12-01 09:59:48 +0700

    refactor: update bot edit form to use new data structure and improve dialog handling

--- src/app/containers/bot-edit-form-container/bot-edit-form-container.ts
+++ src/app/containers/bot-edit-form-container/bot-edit-form-container.ts
@@ -19,10 +19,8 @@ export class BotEditFormContainer {
   public data = inject(MAT_DIALOG_DATA) as {
     title: string
     botData: IBotInfo,
-    botTypesList: string[],
-    botTitleList: string,
-    jobTypesList: string[],
-    jobTitleList: string,
+    botInfoTitle: string
+    jobInfoTitle: string,
   };
 
   newData: any = {...this.data.botData};
@@ -31,8 +29,6 @@ export class BotEditFormContainer {
     if ($event.event === 'BotEditForm:CHANGE_DATA') {
       this.newData = $event.data
     }
-    console.log($event)
-    console.log(this.newData)
   }
 
   events($event: any) {
